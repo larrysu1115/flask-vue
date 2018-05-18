ecaad6e6a6edf12beb45f914d20261b0d1d9523e
larrysu1115 <larrysu1115@gmail.com>
2018-05-18 17:06:54 +0800

    updates

--- b2e/dodo/app2.py
+++ b2e/dodo/app2.py
@@ -6,11 +6,6 @@ def create_app():
     app = Flask(__name__, static_folder='./')
     app.register_blueprint(blueprint_api, url_prefix='/doggy/api')
     app.register_blueprint(blueprint_pages, url_prefix='/doggy/pages')
-
-    @app.route('/')
-    def root():
-        data = dict(say="hello")
-        return jsonify(data), 200
     return app
 
 if __name__ == '__main__':
